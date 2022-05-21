4932c0cb4c313983a514763adac5c6c8a2f2df80
furmen42 <mfurmanczak@gmail.com>
2022-05-21 14:50:21 +0200

    testing set

--- main.py
+++ main.py
@@ -78,12 +78,12 @@ for i in range(1,24):
     # print(np.info(object=bestfeatures))
 
     #build the model
-    Model1 = LogisticRegression(solver='liblinear', random_state=0)
-    Model1.fit(x_train, y_train)
+    Model = LogisticRegression(solver='liblinear', random_state=0)
+    Model.fit(x_train, y_train)
 
-    score = Model1.score(x_test, y_test)
+    score = Model.score(x_test, y_test)
     #make predictions
-    predictions1 = Model1.predict(x_test)
+    predictions1 = Model.predict(x_test)
     # print(predictions1)
     mse = mean_absolute_error(y_test, predictions1)
     # print(mse)
@@ -113,7 +113,7 @@ sc.plot(x='indexes', y='accuracies')
 # scoring = 'accuracy'
 
 # kfold = KFold(n_splits=num_folds, random_state=None)
-# cv_results = cross_val_score(Model1, x_train, y_train, scoring='accuracy', cv=kfold)
+# cv_results = cross_val_score(Model, x_train, y_train, scoring='accuracy', cv=kfold)
 # msg = '%f (%f)'%(cv_results.mean(), cv_results.std())
 # print(msg)
 
@@ -122,4 +122,14 @@ results = pd.DataFrame({'y_test':y_test, 'predictions1':predictions1})
 results.plot.scatter(x='y_test', y='predictions1')
 
 #show plots
-plt.show()
+# plt.show()
+#testing model
+testing = pd.read_excel('data/testingset.xlsx')
+testing = testing.drop('Patient Id', axis = 1)
+
+testarray = testing.values
+Z = testarray[:, 0:23]
+print(Z)
+
+test_results = Model.predict(Z)
+print(test_results)
